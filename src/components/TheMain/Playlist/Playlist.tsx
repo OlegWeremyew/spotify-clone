5678845126edf04db477f8f75629abd7d5f1ece1
olegweremey <olegweremey1994@mail.ru>
2023-04-14 00:48:49 +0300

    forward ref

--- src/components/TheMain/Playlist/Playlist.tsx
+++ src/components/TheMain/Playlist/Playlist.tsx
@@ -1,4 +1,4 @@
-import {FC, useState, MouseEvent} from "react";
+import {FC, useState, MouseEvent, useRef, forwardRef, useEffect} from "react";
 import {PlaylistCover} from "./PlaylistCover";
 import {PlaylistButtonPlay} from "./PlaylistButtonPlay";
 import {PlaylistTitle} from "./PlaylistTitle";
@@ -7,80 +7,113 @@ import {PlaylistContextMenu} from "./PlaylistContextMenu";
 import {SubMenuItem} from "./types";
 
 const menuItems: SubMenuItem[] = [
-    {
-        label: 'Add to Your Library',
+  {
+    label: 'Add to Your Library',
+    subMenuItems: null,
+  },
+  {
+    label: 'Share',
+    subMenuItems: [
+      {
+        label: 'Copy link to playlist',
         subMenuItems: null,
-    },
-    {
-        label: 'Share',
-        subMenuItems: [
-            {
-                label: 'Copy link to playlist',
-                subMenuItems: null,
-            },
-            {
-                label: 'Embed playlist',
-                subMenuItems: null,
-            },
-        ],
-    },
-    {
-        label: 'About recommendations',
+      },
+      {
+        label: 'Embed playlist',
         subMenuItems: null,
-    },
-    {
-        label: 'Open in Desktop app',
-        subMenuItems: null,
-    },
+      },
+    ],
+  },
+  {
+    label: 'About recommendations',
+    subMenuItems: null,
+  },
+  {
+    label: 'Open in Desktop app',
+    subMenuItems: null,
+  },
 ];
 
 export interface IList {
-    classes: string,
-    title: string,
-    description: string,
-    coverUrl: string,
+  classes: string
+  title: string
+  description: string
+  coverUrl: string
 }
 
-export const Playlist: FC<IList> = (
-    {
-        classes,
-        coverUrl,
-        title,
-        description,
-    }
+export type ClickPositionType = {
+  x: null | number
+  y: null | number
+}
+
+const clickPosition: ClickPositionType = {
+  x: null,
+  y: null
+}
+
+export const Playlist: FC<IList> = forwardRef((
+  {
+    classes,
+    coverUrl,
+    title,
+    description,
+  }
 ) => {
 
-    const [isContextMenuOpen, setIsContextMenuOpen] = useState(false);
+  const [isContextMenuOpen, setIsContextMenuOpen] = useState(false);
 
-    const openContextMenu = (event: MouseEvent<HTMLAnchorElement>): void => {
-        event.preventDefault();
-        setIsContextMenuOpen(true);
+  const contextMenuRef = useRef<HTMLUListElement | null>(null)
+
+  const bgClasses = isContextMenuOpen ? 'bg-[#272727]' : 'bg-[#181818] hover:bg-[#272727]'
+
+  const openContextMenu = (event: MouseEvent<HTMLAnchorElement>): void => {
+    event.preventDefault();
+
+    clickPosition.x = event.clientX
+    clickPosition.y = event.clientY
+
+    setIsContextMenuOpen(true);
+  }
+
+  const closeContextMenu = (): void => {
+    setIsContextMenuOpen(false);
+  }
+
+  const updateContextMenuPosition = (): void => {
+    if (contextMenuRef.current) {
+      contextMenuRef.current.style.top = `${clickPosition.y}px`
+      contextMenuRef.current.style.left = `${clickPosition.x}px`
     }
+  }
 
-    const closeContextMenu = (): void => {
-        setIsContextMenuOpen(false);
+  useEffect(() => {
+    if (isContextMenuOpen) {
+      updateContextMenuPosition()
     }
+  })
 
-    return (
-        <a
-            href="/"
-            className={`relative p-4 rounded-md bg-[#181818] hover:bg-[#272727] duration-200 group ${classes}`}
-            onContextMenu={openContextMenu}
-        >
-            <div className="relative">
-                <PlaylistCover url={coverUrl}/>
-                <PlaylistButtonPlay/>
-            </div>
-            <PlaylistTitle title={title}/>
-            <PlaylistDescription description={description}/>
+  return (
+    <a
+      href="/"
+      className={`relative p-4 rounded-md duration-200 group ${classes} ${bgClasses}`}
+      onClick={(event: MouseEvent) => event.preventDefault()}
+      onContextMenu={openContextMenu}
+    >
+      <div className="relative">
+        <PlaylistCover url={coverUrl}/>
+        <PlaylistButtonPlay/>
+      </div>
+      <PlaylistTitle title={title}/>
+      <PlaylistDescription description={description}/>
 
-            {isContextMenuOpen && (
-                <PlaylistContextMenu
-                    menuItems={menuItems}
-                    classes="absolute top-9 left-9 bg-[#282828] text-[#eaeaea] text-sm divide-y divide-[#3e3e3e] p-1 rounded shadow-xl cursor-default whitespace-nowrap z-10"
-                    onClose={closeContextMenu}
-                />
-            )}
-        </a>
-    );
-}
+      {isContextMenuOpen && (
+        <PlaylistContextMenu
+          ref={contextMenuRef}
+          menuItems={menuItems}
+          classes="fixed bg-[#282828] text-[#eaeaea] text-sm divide-y divide-[#3e3e3e] p-1 rounded shadow-xl cursor-default whitespace-nowrap z-10"
+          onClose={closeContextMenu}
+        />
+      )}
+    </a>
+  );
+})
--- src/components/TheMain/Playlist/PlaylistContextMenu/PlaylistContextMenu.tsx
+++ src/components/TheMain/Playlist/PlaylistContextMenu/PlaylistContextMenu.tsx
@@ -1,37 +1,55 @@
-import {FC, useEffect} from "react";
+import {FC, ForwardedRef, forwardRef, useEffect} from "react";
 import {PlaylistContextMenuItem} from "./PlaylistContextMenuItem";
 import {SubMenuItem} from "../types";
 
 export interface IContextMenu {
-    menuItems: SubMenuItem[]
-    classes: string
-    onClose?: () => void
+  menuItems: SubMenuItem[]
+  classes: string
+  onClose?: () => void
+  ref: ForwardedRef<HTMLUListElement>
 }
 
-export const PlaylistContextMenu: FC<IContextMenu> = (
-    {
-        classes,
-        menuItems,
-        onClose = () => {
-        },
+//if callback ONCLOSE was not transferred
+const mockFn = () => {
+}
+
+export const PlaylistContextMenu: FC<IContextMenu> = forwardRef((
+  {
+    classes,
+    menuItems,
+    onClose: handleClose = mockFn
+  }, ref) => {
+
+
+  useEffect(() => {
+    if (!handleClose) return
+
+    const handleClickAway = (event: MouseEvent) => {
+      // @ts-ignore
+      if (!ref.current?.contains(event.target as Node)) {
+        handleClose()
+      }
     }
-) => {
 
-    useEffect(() => {
-        document.addEventListener('mousedown', onClose);
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        handleClose()
+      }
+    }
 
-        return () => {
-            document.removeEventListener('mousedown', onClose);
-        };
-    });
+    document.addEventListener('mousedown', handleClickAway);
+    document.addEventListener('keydown', handleEsc);
 
-    return (
-        <ul className={classes}>
-            {menuItems.map(({label, subMenuItems}: SubMenuItem) => (
-                <PlaylistContextMenuItem key={label} subMenuItems={subMenuItems}>
-                    {label}
-                </PlaylistContextMenuItem>
-            ))}
-        </ul>
-    );
-}
+    return () => document.removeEventListener('mousedown', handleClickAway);
+  });
+
+  return (
+    <ul className={classes} ref={ref}>
+      {menuItems.map(({label, subMenuItems}: SubMenuItem) => (
+        <PlaylistContextMenuItem key={label} subMenuItems={subMenuItems}>
+          {label}
+        </PlaylistContextMenuItem>
+      ))}
+    </ul>
+  );
+})
